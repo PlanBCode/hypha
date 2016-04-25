ad67d585dbb026fc9ffd53eb4e50cc0c7d51a18d
Matthijs Kooijman <matthijs@stdin.nl>
2016-04-25 11:59:45 +0200

    Mark words in the addPage dialog as translatable
    The actual translations for these words already exist, so no need to add
    them.

--- system/core/pages.php
+++ system/core/pages.php
@@ -52,8 +52,8 @@
 	}
 	function newPage() {
 		html = '<table class="section"><tr><th colspan="2"><?=__('create-new-page')?></td><tr>';
-		html+= '<tr><th>type</th><td><select id="newPageType" name="newPageType">' + '<?php foreach($types as $type) echo '<option value="'.$type.'"'.($type=='textPage' ? 'selected="selected"' : '').'>'.$type.'</option>'; ?>' + '</select></td></tr>';
-		html+= '<tr><th>name</th><td><input type="text" id="newPagename" value="<?=hypha_getPage($query[0], $query[1]) ? '' : $query[1]?>" onblur="validatePagename(this);" onkeyup="validatePagename(this); document.getElementById(\'newPageSubmit\').disabled = this.value ? false : true;"/></td></tr>';
+		html+= '<tr><th><?=__('type')?></th><td><select id="newPageType" name="newPageType">' + '<?php foreach($types as $type) echo '<option value="'.$type.'"'.($type=='textPage' ? 'selected="selected"' : '').'>'.$type.'</option>'; ?>' + '</select></td></tr>';
+		html+= '<tr><th><?=__('name')?></th><td><input type="text" id="newPagename" value="<?=hypha_getPage($query[0], $query[1]) ? '' : $query[1]?>" onblur="validatePagename(this);" onkeyup="validatePagename(this); document.getElementById(\'newPageSubmit\').disabled = this.value ? false : true;"/></td></tr>';
 		html+= '<tr><td></td><td><input type="checkbox" id="newPagePrivate" name="newPagePrivate"/> <?=__('private-page')?></td></tr>';
 		html+= '<tr><td></td><td><input type="button" class="button" value="<?=__('cancel')?>" onclick="document.getElementById(\'popup\').style.visibility=\'hidden\';" />';
 		html+= '<input type="submit" id="newPageSubmit" class="button editButton" value="<?=__('create')?>" disabled="true" onclick="hypha(\'<?=$hyphaLanguage?>/\' + document.getElementById(\'newPagename\').value + \'/edit\', \'newPage\', document.getElementById(\'newPageType\').value + \',\' + (document.getElementById(\'newPagePrivate\').checked ? \'on\' : \'off\'));" /></td></tr></table>';
