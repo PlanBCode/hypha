PlanBCode/hypha

commit 08ecabbd7ac352e32dfaa8da1949fc85a377756c

Preserve custom link texts and titles
Previously, during wikify the link text and title would be
unconditionally cleared. During dewikify, the link text would be
replaced with the page name, and in some circumstances the title would
also be set tor overwritten.

Now, the link text is only cleared by wikify when it equals the page
name, and set to the pagename by dewikify when it was empty, effectively
preserving any custom link text. For the title, this works similarly.

In addition, the wymeditor link editing code is changed to pull the page
name from the url instead of from the link text, as these are not
necessarily the same anymore. This also fixes link editing, which was
broken since urls in links were made relative.

system/core/pages.php
@@ -316,7 +316,8 @@
 
 		// Use the page name (in the appropriate language) as
 		// the link text
-		$node->text(showPagename($language->getAttribute('name')));
+		if ($node->text() == '')
+			$node->text(showPagename($language->getAttribute('name')));
 
 		// Check permissions, replace by a span with just the
 		// pagename if the link would lead to an inaccessible
@@ -330,7 +331,8 @@
 
 		// Add appropriate class and/or title attributes
 		if ($language->getAttribute('id') != $hyphaLanguage) {
-			$node->setAttribute('title', __('page-in-other-language'));
+			if (!$node->getAttribute('title'))
+				$node->setAttribute('title', __('page-in-other-language'));
 			$node->addClass('otherLanguageLink');
 		} else if ($language->getAttribute('name') == $hyphaPage->pagename) {
 			$node->addClass('currentPageLink');
@@ -409,11 +411,16 @@
 		$node->removeClass('currentPageLink');
 		if ($node->getAttribute('class') == '')
 			$node->removeAttribute('class');
-		$node->removeAttribute('title');
+		if ($node->getAttribute('title') == __('page-in-other-language'))
+			$node->removeAttribute('title');
 
 		$uri = $page ? 'hypha:' . $page->getAttribute('id') : '';
 		$node->setAttribute('href', $uri);
-		$node->text('');
+
+		// Clear out the page name in the link text, but keep
+		// any custom link name
+		if ($node->text == showPagename($language->getAttribute('name')))
+			$node->text('');
 	}
 
 	/*

system/core/editor.php
@@ -181,11 +181,14 @@
 
 		// if existing link is edited retrieve link parameters
 		if (selObj) {
-			if (selObj.attr('href').indexOf('<?=$hyphaUrl?>')==0) {
+			var href = selObj.attr('href').replace('<?=$hyphaUrl?>', '');
+			var match = /^<?=$hyphaLanguage?>\/([^\/]*)$/.exec(href);
+			if (match !== null) {
 				wdw.$('#selectLinkSourceLocal').attr('checked', 'checked');
 				jQuery(body).filter('.wym_dialog_link').find('.wymLinkUrl').css("display", "none");
 				jQuery(body).filter('.wym_dialog_link').find('.wymLinkLocal').css("display", "block");
-				wdw.$('#localLinkName').val(selObj.html());
+				pagename = match[1];
+				wdw.$('#localLinkName').val(pagename);
 			}
 			else {
 				wdw.$('#selectLinkSourceUrl').attr('checked', 'checked');

commit 546e9889d72b1dc69a11e50ab30795933d127d8f

Fix dewikify_link
This was broken in 08ecabb (Preserve custom link texts and titles), it
looks like some copy-paste or rebase that was not really tested.

system/core/pages.php
@@ -419,7 +419,8 @@
 
 		// Clear out the page name in the link text, but keep
 		// any custom link name
-		if ($node->text == showPagename($language->getAttribute('name')))
+		$lang = hypha_pageGetLanguage($page, $language);
+		if ($node->text() == showPagename($lang->getAttribute('name')))
 			$node->text('');
 	}
 