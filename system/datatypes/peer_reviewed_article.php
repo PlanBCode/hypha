7c4e7ef2a1941e8ce4b60bb8ef9df0bf7a2c4364
Bezuur <41150116+Bezuur@users.noreply.github.com>
2018-11-29 14:18:25 +0100

    Repeat some buttons at end of article page
    For better understanding some buttons and a message are added at end of
    the article page. This better supports the workflow, especially since
    the article editing interface is a bit longer than most others.

--- system/datatypes/peer_reviewed_article.php
+++ system/datatypes/peer_reviewed_article.php
@@ -533,6 +533,7 @@ class peer_reviewed_article extends Page {
 
 		if (isUser()) {
 			$commands = $this->findBySelector('#pageCommands');
+			$commandsAtEnd = $this->findBySelector('#pageEndCommands');
 			$commands->append($this->makeActionButton(__('edit'), self::PATH_EDIT));
 
 			// the status change from review to approved is done automatically
@@ -540,6 +541,7 @@ class peer_reviewed_article extends Page {
 				$userId = $this->hyphaUser->getAttribute('id');
 				if (!$this->hasUserApproved($userId)) {
 					$commands->append($this->makeActionButton(__('art-approve'), self::PATH_APPROVE));
+					$commandsAtEnd->append($this->makeActionButton(__('art-approve'), self::PATH_APPROVE));
 				}
 			} else {
 				foreach ($this->statusMtx[$status] as $newStatus => $option) {
@@ -1088,6 +1090,9 @@ EOF;
 		$commands = $this->findBySelector('#pageCommands');
 		$commands->append($this->makeActionButton(__('art-cancel')));
 		$commands->append($this->makeActionButton(__('art-save'), self::PATH_EDIT, self::FORM_CMD_EDIT));
+		$commandsAtEnd = $this->findBySelector('#pageEndCommands');
+		$commandsAtEnd->append($this->makeActionButton(__('art-cancel')));
+		$commandsAtEnd->append($this->makeActionButton(__('art-save'), self::PATH_EDIT, self::FORM_CMD_EDIT));
 
 		return $this->createForm($elem, $data);
 	}
