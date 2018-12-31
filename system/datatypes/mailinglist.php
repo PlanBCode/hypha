PlanBCode/hypha

commit 0e42217d64debb32a9d192da690fba994bed6a67

Add test send option to mailinglist
This fixes #140.

system/datatypes/mailinglist.php
@@ -32,6 +32,7 @@ class mailinglist extends Page {
 	const FORM_CMD_LIST_DELETE = 'delete';
 	const FORM_CMD_MAILING_SAVE = 'save';
 	const FORM_CMD_MAILING_SEND = 'send';
+	const FORM_CMD_MAILING_TEST_SEND = 'test_send';
 
 	const MAILING_STATUS_DRAFT = 'draft';
 	const MAILING_STATUS_SENT = 'sent';
@@ -518,9 +519,10 @@ class mailinglist extends Page {
 	 * @return array|null
 	 */
 	private function showMailingAction($mailingId) {
-		if ($this->isPosted(self::FORM_CMD_MAILING_SEND)) {
+		if ($this->isPosted(self::FORM_CMD_MAILING_SEND) || $this->isPosted(self::FORM_CMD_MAILING_TEST_SEND)) {
 			try {
-				$this->sendMailing($mailingId);
+				if ($this->isPosted(self::FORM_CMD_MAILING_SEND)) $this->sendMailing($mailingId);
+				if ($this->isPosted(self::FORM_CMD_MAILING_TEST_SEND)) $this->testSendMailing($mailingId‚ $_POST['argument']);
 				$msg = ucfirst(__('ml-successfully-sent'));
 				$msgType = 'success';
 			} catch (\Exception $e) {
@@ -577,6 +579,7 @@ class mailinglist extends Page {
 		if ($inDraft) {
 			$commands->append($this->makeActionButton(__('edit'), $mailingId . '/edit'));
 			$commands->append($this->makeActionButton(__('send'), $mailingId, self::FORM_CMD_MAILING_SEND));
+			$commands->append(makeButton(__('ml-test-send'), 'hypha(\''.$path.'\', \''.self::FORM_CMD_MAILING_TEST_SEND.'\', prompt(\'' . __('email') . '\'));'));
 		}
 
 		return null;
@@ -761,6 +764,19 @@ EOF;
 		$this->xml->saveAndUnlock();
 
 		return $mailing;
+	}
+
+	private function testSendMailing($mailingId, $email) {
+		if (!$this->hasSender()) {
+			throw new \Exception(__('ml-no-sender'));
+		}
+
+		$senderName = $this->getDoc()->getAttribute('sender-name');
+		$senderEmail = $this->getDoc()->getAttribute('sender-email');
+
+		/** @var HyphaDomElement $mailing */
+		$mailing = $this->xml->document()->getElementById($mailingId);
+		$this->send($mailing->getAttribute('subject'), $mailing->getHtml(), [$email], $senderEmail, $senderName);
 	}
 
 	/**

system/languages/en.php
@@ -239,6 +239,7 @@
 		"datatype.name.mailinglist" => "mailinglist",
 
 		// mailing list
+		"ml-test-send" => "test-send",
 		"ml-view-addresses" => "view addresses",
 		"ml-date-format" => "d/m/Y",
 		"ml-received-by" => "received by",

system/languages/nl.php
@@ -234,6 +234,7 @@
 		"datatype.name.mailinglist" => "mailinglist",
 
 		// mailing list
+		"ml-test-send" => "test-verzending",
 		"ml-view-addresses" => "bekijk adressen",
 		"ml-date-format" => "d-m-Y",
 		"ml-received-by" => "ontvangen door",

commit bf2d2b4729779f23425df32cd80662cb3c4d8142

Fix syntax error in mailinglist
Somehow there was a weird unicode comma-character in there, rather than
a normal comma, which completely breaks Hypha.

This was broken in commit 0e42217 (Add test send option to mailinglist).

system/datatypes/mailinglist.php
@@ -522,7 +522,7 @@ class mailinglist extends Page {
 		if ($this->isPosted(self::FORM_CMD_MAILING_SEND) || $this->isPosted(self::FORM_CMD_MAILING_TEST_SEND)) {
 			try {
 				if ($this->isPosted(self::FORM_CMD_MAILING_SEND)) $this->sendMailing($mailingId);
-				if ($this->isPosted(self::FORM_CMD_MAILING_TEST_SEND)) $this->testSendMailing($mailingId‚ $_POST['argument']);
+				if ($this->isPosted(self::FORM_CMD_MAILING_TEST_SEND)) $this->testSendMailing($mailingId, $_POST['argument']);
 				$msg = ucfirst(__('ml-successfully-sent'));
 				$msgType = 'success';
 			} catch (\Exception $e) {